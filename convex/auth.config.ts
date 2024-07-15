9e1d1c75773ddf5dc8d7bc8081418a2942728448
chronoll <kurotaka.wa@gmail.com>
2024-07-15 16:50:54 +0900

    setting convex(clerk) authentication

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+/* trunk-ignore-all(eslint/import/no-anonymous-default-export) */
+export default {
+  providers: [
+    {
+      domain: 'https://first-woodcock-10.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
